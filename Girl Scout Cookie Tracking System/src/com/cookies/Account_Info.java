bea7cce0d46dae34d4fa42053fbde04eb226d52e
Jimmy Bowens <jimmy@jimmybowens.com>
2014-10-09 12:22:17 -0400

    Added Account_Info and Login classes

---
+++ Girl Scout Cookie Tracking System/src/com/cookies/Account_Info.java
@@ -0,0 +1,8 @@
+package com.cookies;
+
+public class Account_Info {
+	public Account_Info(){
+		
+	}
+
+}
--- Girl Scout Cookie Tracking System/src/com/cookies/Start_Program.java
+++ Girl Scout Cookie Tracking System/src/com/cookies/Start_Program.java
@@ -5,11 +5,7 @@ import java.sql.*;
 
 import javax.swing.*;
 
-public class Start_Program extends JPanel{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+public class Start_Program{
 	public static Connection connection = null;
 	public static Boolean isUserValidated = false;
 	
@@ -17,25 +13,6 @@ public class Start_Program extends JPanel{
 		
 	}
 	
-	public static boolean validateLogin() throws SQLException{
-		//String url = "jdbc:mysql://192.186.249.9/PenFed_Realty";
-		String url = "jdbc:mysql://localhost/Java_Database_Project";
-		//String user ="administrator";
-		String user = "root";
-		//String pass = "adminpass";
-		String pass = "admin";
-		
-		try{
-			connection = DriverManager.getConnection(url,user,pass);
-		}catch (SQLException e){
-			System.out.println(e);
-		}finally{
-			connection.close();
-		}
-		
-		
-		return isUserValidated;
-	}
 	
 	public static void main(String[] args) throws SQLException {
 		JFrame mainFrame = new JFrame();
@@ -43,8 +20,10 @@ public class Start_Program extends JPanel{
 		mainFrame.setVisible(true);
 		mainFrame.setSize(new Dimension(800,800));
 		
-		Start_Program.validateLogin();
-		System.out.println(Start_Program.isUserValidated);
+		mainFrame.getContentPane().add(new Login("something","something"));
+		
+		Login.validateLogin();
+		//System.out.println(Start_Program.isUserValidated);
 	}
 
 }
